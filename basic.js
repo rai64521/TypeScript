e0b98dd0227a9cf1d32916e59d0b755802988321
rai64521 <raisumant99@gmail.com>
2025-12-04 21:02:33 +0530

    basic of typescript

---
+++ basic.js
@@ -0,0 +1,6 @@
+function greet(name) {
+    return "Hello ".concat(name);
+}
+console.log(greet("Sumant"));
+// console.log(greet(101));
+// console.log(greet(true));
---
+++ basic.ts
@@ -0,0 +1,9 @@
+function greet(name: string): string{
+    return `Hello ${name}`
+}
+
+
+console.log(greet("Sumant"));
+// console.log(greet(101));
+// console.log(greet(true));
+
